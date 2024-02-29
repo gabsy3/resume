feba4cc8e5b96121fd25c85a9999ca654ecbd9eb
gaby <gabsy3@gmail.com>
2024-02-29 15:02:42 +0200

    portfolio

--- resume/src/app/app.routes.ts
+++ resume/src/app/app.routes.ts
@@ -6,7 +6,7 @@ import { PortfolioComponent } from './components/portfolio/portfolio.component';
 
 export const routes: Routes = [
     {path:'',component:HomeComponent},
-    {path:'**',component:HomeComponent},
+    {path:'home',component:HomeComponent},
     {path:'about',component:AboutComponent},
     {path:'contact',component:ContactComponent},
     {path:'portfolio',component:PortfolioComponent},
--- resume/src/app/components/header/header.component.ts
+++ resume/src/app/components/header/header.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [MatToolbarModule , RouterLink , RouterLinkActive],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
