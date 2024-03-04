f7ec10dab2234ca12e7b1a86b5e03b20b17013a6
gaby <gabsy3@gmail.com>
2024-03-04 14:10:26 +0200

    portfolio

--- resume/src/app/components/contact/contact.component.ts
+++ resume/src/app/components/contact/contact.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import emailjs, { type EmailJSResponseStatus } from '@emailjs/browser';
+
 
 @Component({
   selector: 'app-contact',
@@ -8,5 +10,20 @@ import { Component } from '@angular/core';
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
+  public sendEmail(e: Event) {
+    e.preventDefault();
 
+    emailjs
+      .sendForm('service_nna5qfv', 'template_i8nne2m', e.target as HTMLFormElement, {
+        publicKey: 'YEQiExwdbJKy1Yvvr',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', (error as EmailJSResponseStatus).text);
+        },
+      );
+  }
 }
