cff5fd3e9c766410d82347702222cf426b473161
gaby <gabsy3@gmail.com>
2024-04-18 11:48:43 +0300

    portfolio

--- resume/src/app/components/portfolio/portfolio.component.ts
+++ resume/src/app/components/portfolio/portfolio.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { PdfViewerModule } from 'ng2-pdf-viewer';
 
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [],
+  imports: [PdfViewerModule],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.scss'
 })
 export class PortfolioComponent {
-
 }
